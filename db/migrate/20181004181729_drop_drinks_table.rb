6969e9ac09f6d4b1313d67cffdb40b7fcf2626ee
Leo Generali <leogen17@gmail.com>
2018-10-04 20:19:50 -0400

    Fix issues with migrations

--- db/migrate/20181004181729_drop_drinks_table.rb
+++
@@ -1,9 +0,0 @@
-class DropDrinksTable < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :drinks
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_181729) do
+ActiveRecord::Schema.define(version: 2018_10_04_181458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
